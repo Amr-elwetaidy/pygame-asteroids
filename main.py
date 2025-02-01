28bb1ce0bbeb76dc0b20defb388bdbbeccc0e656
Amr-elwetaidy <amr.elwetaidy@gmail.com>
2025-02-01 02:10:35 +0000

    Add player and circle shape classes with basic rendering

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ import pygame
 import os
 
 from constants import *
+from player import Player
+
+os.environ["LIBGL_ALWAYS_INDIRECT"] = "1"
+os.environ["SDL_AUDIODRIVER"] = "dummy"
+os.environ["DISPLAY"] = "host.docker.internal:0"
 
 
 def main():
-    os.environ["SDL_VIDEODRIVER"] = "dummy"
-
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-    # initialize pygame
     pygame.init()
 
-    # create a window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
     # run the game loop
     while True:
@@ -26,9 +28,12 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
